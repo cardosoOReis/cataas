49607fa7a025be14cd364e37be97a248d04cd244
cardosoOReis <rafaelcdsreis@gmail.com>
2023-02-05 17:14:11 -0300

    refactor to cat_repository_impl to avoid code repetition

--- lib/features/cataas/data/repositories/cat_repository_impl.dart
+++ lib/features/cataas/data/repositories/cat_repository_impl.dart
@@ -1,15 +1,14 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:cataas/core/error/exceptions.dart';
-import 'package:cataas/features/cataas/domain/usecases/get_cat_by_id_usecase.dart';
-import 'package:cataas/features/cataas/domain/usecases/get_cat_by_tag_usecase.dart';
-import 'package:cataas/features/cataas/domain/usecases/get_random_cat_usecase.dart';
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/network/i_network_info.dart';
+import '../datasources/i_cat_remote_datasource.dart';
+import '../models/cat_model.dart';
+import '../../domain/entities/cat_entity.dart';
+import '../../domain/repositories/i_cat_repository.dart';
+import '../../domain/usecases/get_cat_by_id_usecase.dart';
+import '../../domain/usecases/get_cat_by_tag_usecase.dart';
+import '../../domain/usecases/get_random_cat_usecase.dart';
 import 'package:dartz/dartz.dart';
-
-import 'package:cataas/core/error/failures.dart';
-import 'package:cataas/core/platform/i_network_info.dart';
-import 'package:cataas/features/cataas/data/datasources/i_cat_remote_datasource.dart';
-import 'package:cataas/features/cataas/domain/entities/cat_entity.dart';
-import 'package:cataas/features/cataas/domain/repositories/i_cat_repository.dart';
 
 class CatRepositoryImpl implements ICatRepository {
   final ICatRemoteDatasource remoteDatasource;
@@ -23,50 +22,27 @@ class CatRepositoryImpl implements ICatRepository {
   Future<Either<Failure, CatEntity>> getCatById(
     GetCatByIdUsecaseParams params,
   ) async {
-    if (await networkInfo.isConnected) {
-      try {
-        final result = await remoteDatasource.getCatById(params);
-
-        return Right(result);
-      } on CatNotFoundException catch (e) {
-        return Left(ApiFailure(exception: e));
-      } on ParseDataException catch (e) {
-        return Left(ParseDataFailure(exception: e));
-      } catch (_) {
-        rethrow;
-      }
-    } else {
-      return Left(NoInternetConnectionFailure());
-    }
+    return await _getCat(() => remoteDatasource.getCatById(params));
   }
 
   @override
   Future<Either<Failure, CatEntity>> getCatByTag(
       GetCatByTagUsecaseParams params) async {
-    if (await networkInfo.isConnected) {
-      try {
-        final result = await remoteDatasource.getCatByTag(params);
-
-        return Right(result);
-      } on CatNotFoundException catch (e) {
-        return Left(ApiFailure(exception: e));
-      } on ParseDataException catch (e) {
-        return Left(ParseDataFailure(exception: e));
-      } catch (_) {
-        rethrow;
-      }
-    } else {
-      return Left(NoInternetConnectionFailure());
-    }
+    return await _getCat(() => remoteDatasource.getCatByTag(params));
   }
 
   @override
   Future<Either<Failure, CatEntity>> getRandomCat(
     GetRandomCatUsecaseParams params,
   ) async {
+    return await _getCat(() => remoteDatasource.getRandomCat(params));
+  }
+
+  Future<Either<Failure, CatEntity>> _getCat<Params>(
+      Future<CatModel> Function() getCat) async {
     if (await networkInfo.isConnected) {
       try {
-        final result = await remoteDatasource.getRandomCat(params);
+        final result = await getCat();
 
         return Right(result);
       } on CatNotFoundException catch (e) {
